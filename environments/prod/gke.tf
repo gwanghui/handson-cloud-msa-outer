1e95ca63632f603f9dc7dcd444aa170b078b4556
gwanghui <park776139@gmail.com>
2020-11-27 16:08:10 +0900

    remove interpolation-expression

--- environments/prod/gke.tf
+++ environments/prod/gke.tf
@@ -44,7 +44,7 @@ resource "google_container_cluster" "primary" {
  
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}"
+  name       = google_container_cluster.primary.name
   location   = var.region
   cluster    = google_container_cluster.primary.name
  
